567e8e4f1860734d90a13931b741c2eb6a0c6eb0
Vitor Brito <vitorlsbrito@gmail.com>
2021-03-17 20:18:54 +0000

    Amazon

--- pages/api/stops/index.js
+++ pages/api/stops/index.js
@@ -4,27 +4,12 @@ async function Stops (req, res) {
     const { method } = req;
 
     switch (method) {
-        case method == 'GET':
+        case 'GET':
             const stops = await Stop.scan().exec();
             return res.status(200).json(stops);
-            break;
         default:
             return res.status(200).json({ message: 'Teste' });
     }
-
-    /*
-    switch (method) {
-        case method == 'GET':
-            const stops = await Stop.scan().exec();
-
-            return res.status(200).json({ message: 'AOEW' });
-            //return res.status(200).json({ message: 'GET' });
-        case method == 'POST':
-            return res.status(200).json({ message: 'POST' });
-        default:
-            return res.status(200).json({ message: 'TESTE!' });
-
-    */
 }
 
 export default Stops;
